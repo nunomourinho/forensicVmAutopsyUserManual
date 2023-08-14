7d02a17c0627f0aaee9bc0d2e13c7863b498d471
Nuno Mourinho <56609351+nunomourinho@users.noreply.github.com>
2023-08-14 08:12:20 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@ extensions = [
     'sphinx.ext.autodoc',    
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',	
+    'sphinx.ext.autosectionlabel',
     #'sphinx_autodoc_typehints',
     #'sphinxcontrib.autodocsumm',
 ]
